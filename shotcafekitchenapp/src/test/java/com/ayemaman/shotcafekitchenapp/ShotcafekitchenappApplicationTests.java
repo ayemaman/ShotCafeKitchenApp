0324f303471e26a7f3eac3c526ca6167fd266228
Alex <aleksandrs.gurcenko@gmail.com>
2021-11-13 11:14:40 +0100

    first commit

---
+++ shotcafekitchenapp/src/test/java/com/ayemaman/shotcafekitchenapp/ShotcafekitchenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayemaman.shotcafekitchenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShotcafekitchenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
